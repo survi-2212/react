cc6e144fde5326e6e379f3899107473fc1876e90
Suravi Singh <suravisingh331@gmail.com>
2022-03-11 17:41:20 +0530

    to do list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ArithmeticOperation from "./ArithmeticOperation/ArithmeticOperation"; 
+import Assignment from "./To_Do/Assignment";
 
 class App extends React.Component{
   render(){
     return(
       <div>
-        <ArithmeticOperation/>
+        {/* <ArithmeticOperation/> */}
+        <Assignment/>
       </div>
     )
   }
---
+++ src/To_Do/Assignment.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+export default class Assignment extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            taskArr:[],
+            taskInput:"",
+            taskcomplete:[]
+        }
+    }
+
+    handleinput=(event)=>{
+        this.setState({
+            taskInput:event.target.value
+        })
+    }  
+    handlepush =()=>{
+        var temp=this.state.taskArr
+        temp.push(this.state.taskInput)
+        this.setState({
+            taskArr:temp,
+            taskInput:""
+        })
+        
+    }
+
+    handlecomplete =(index)=>{
+        var temp=this.state.taskArr
+        var temp2=this.state.taskcomplete
+        var temp3=temp.splice(index,1)
+        temp2.push(temp3)
+        this.setState({
+            taskArr:temp,
+            taskcomplete:temp2
+        })
+    }
+    render(){
+        return(
+            <div>
+                <input type="text" onChange={this.handleinput} value={this.state.taskInput}/>
+                {/* <h1>{this.state.taskInput}</h1> */}
+                <button onClick={this.handlepush}>Add Task</button>
+
+                <h2>Tasks to do</h2>
+                <div>
+                    <ol>
+                        {this.state.taskArr.map( (item,index)=>{
+                            return( 
+                                <li>{item}
+                                 <button onClick={()=>{
+                                     this.handlecomplete(index)
+                                 }}>Completed</button>
+                                 </li>                            
+                            )
+                        })}
+                    </ol>
+                </div>
+
+                <h2>Completed Task</h2>
+                <ol>
+                    {this.state.taskcomplete.map( (list)=>{
+                        return(
+                            <li>{list}</li>
+                        )
+                    })}
+                </ol>
+
+            </div>
+        )
+    }
+}
